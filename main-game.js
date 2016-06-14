827bbae80d0bed08abd4084860d6a361fb53d2a1
BKFighter <bkfergen@gmail.com>
2016-06-14 18:08:22 -0500

    Update main-game.js

--- main-game.js
+++ main-game.js
@@ -10,21 +10,7 @@ var container, camera, scene, renderer, mesh,
     CANVAS_WIDTH = 600,
     CANVAS_HEIGHT = 600;
 
-// info
-info = document.createElement( 'div' );
-info.style.position = 'absolute';
-info.style.top = '30px';
-info.style.width = '100%';
-info.style.textAlign = 'center';
-info.style.color = '#f00';
-info.style.backgroundColor = 'transparent';
-info.style.zIndex = '1';
-info.style.fontFamily = 'Monospace';
-info.innerHTML = 'INTERSECT Count: ' + count;
-info.style.userSelect = "none";
-info.style.webkitUserSelect = "none";
-info.style.MozUserSelect = "none";
-document.body.appendChild( info );
+
 
 container = document.getElementById( 'player' );
 document.body.appendChild( container );
@@ -40,7 +26,7 @@ camera.position.y = 150;
 camera.position.z = 500;
 camera.lookAt( scene.position );
 
-var geometry = new THREE.CylinderGeometry( 5, 5, 20, 32 );
+var geometry = new THREE.CylinderGeometry( 100, 100, 40, 32 );
 var material = new THREE.MeshBasicMaterial( {color: 0xffff00} );
 var cylinder = new THREE.Mesh( geometry, material );
 scene.add( cylinder );
