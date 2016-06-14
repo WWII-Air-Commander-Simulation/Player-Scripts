75c4bb4116332fd97bfb52808031c2814881268a
BKFighter <bkfergen@gmail.com>
2016-06-14 14:03:29 -0500

    Update main-game.js

--- main-game.js
+++ main-game.js
@@ -1,1 +1,15 @@
+container = document.getElementById( 'player' );
+document.body.appendChild( container );
 
+renderer = new THREE.WebGLRenderer();
+renderer.setSize( 600, 600 );
+container.appendChild( renderer.domElement );
+
+var scene	= new THREE.Scene();
+var camera	= new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.01, 1000);
+camera.position.z = 5;
+
+var geometry = new THREE.CylinderGeometry( 5, 5, 20, 32 );
+var material = new THREE.MeshBasicMaterial( {color: 0xffff00} );
+var cylinder = new THREE.Mesh( geometry, material );
+scene.add( cylinder );
