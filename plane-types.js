a253486a5a98796d76a2e91560ebdf87937bc647
BKFighter <bkfergen@gmail.com>
2016-06-15 11:14:22 -0500

    Partially done...
    As you can see I started researching but was only parti-

--- plane-types.js
+++ plane-types.js
@@ -2,9 +2,9 @@ function planeType = (displayName, healthPoints, maxSpeed, weight, gunTurrets, r
 
 this.displayName = displayname;
 this.healthPoints = healthPoints;
-this.maxSpeed = maxSpeed;
-this.weight = weight;
-this.gunTurrets = gunTurrets;
+this.maxSpeed = maxSpeed;                  //Required to be in Feet Per Second
+this.weight = weight;                      //Required to be in Pounds (American)
+this.gunTurrets = gunTurrets;              //Number of Turrets not Guns
 this.rpm = rpm;
 this.functionality = functionality;
 this.range = range;
@@ -14,3 +14,6 @@ this.bombPayload = bombPayload;
 this.ammoAmount = ammoAmount;
 
 }
+
+
+//var b17 = new planeType("B-17", 1234, 421, 33279, 8, )
