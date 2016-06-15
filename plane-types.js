858795d8e13ff29d2dceb572b8a62af33091a96f
BKFighter <bkfergen@gmail.com>
2016-06-15 11:04:03 -0500

    Setup

---
+++ plane-types.js
@@ -0,0 +1,16 @@
+function planeType = (displayName, healthPoints, maxSpeed, weight, gunTurrets, rpm, functionality, range, bombDamage, gunDamage, bombPayload, ammoAmount){
+
+this.displayName = displayname;
+this.healthPoints = healthPoints;
+this.maxSpeed = maxSpeed;
+this.weight = weight;
+this.gunTurrets = gunTurrets;
+this.rpm = rpm;
+this.functionality = functionality;
+this.range = range;
+this.bombDamage = bombDamage;
+this.gunDamage = gunDamage;
+this.bombPayload = bombPayload;
+this.ammoAmount = ammoAmount;
+
+}
