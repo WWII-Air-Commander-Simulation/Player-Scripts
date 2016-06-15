34a899dbbf68a3215f758943f9386efbf68e2434
BKFighter <bkfergen@gmail.com>
2016-06-15 17:01:03 -0500

    Update plane-types.js

--- plane-types.js
+++ plane-types.js
@@ -1,19 +1,22 @@
-function planeType = (displayName, healthPoints, maxSpeed, weight, gunTurrets, rpm, functionality, range, bombDamage, gunDamage, bombPayload, ammoAmount){
+function planeType = (displayName, healthPoints, maxSpeed, weight, wingspan, height, planeLength, gunTurrets, rpm, functionality, range, bombDamage, gunDamage, bombPayload, ammoAmount){
 
 this.displayName = displayname;
 this.healthPoints = healthPoints;
 this.maxSpeed = maxSpeed;                  //Required to be in Feet Per Second
 this.weight = weight;                      //Required to be in Pounds (American)
+this.wingspan = wingspan;
+this.height = height;
+this.planeLength = planeLength
 this.gunTurrets = gunTurrets;              //Number of Turrets not Guns
-this.rpm = rpm;
-this.functionality = functionality;
-this.range = range;
+this.rpm = rpm;                            //Rounds per Minute
+this.functionality = functionality;        //"Fighter" or "Bomber"
+this.range = range;                        //In Feet
 this.bombDamage = bombDamage;
 this.gunDamage = gunDamage;
-this.bombPayload = bombPayload;
-this.ammoAmount = ammoAmount;
+this.bombPayload = bombPayload;            //Amount of bombs
+this.ammoAmount = ammoAmount;              //Amount of bullets
 
 }
 
 
-//var b17 = new planeType("B-17", 1234, 421, 33279, 8, )
+var b17 = new planeType("B-17", 1234, 421, 33279, 103, 19, 74, 7, 600, "Bomber", 10560000, 1234, 1234, 4800, 3000)
